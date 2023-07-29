02478342a5ebb440ad040c92fd1e0310f772421b
Nikita Degtyarev <nikita.deki4@gmail.com>
2023-07-29 13:14:28 +0300

    done day12 in Module2. жаль, что предыдущие дни из 2го модуля безвозратно утеряны

---
+++ 2/day13/main.js
@@ -0,0 +1,10 @@
+// let button = document.getElementById('buttonID');
+// button.addEventListener('click', ()=>{
+//     document.body.style.backgroundColor = "red";
+// })
+
+import {add, subtract} from './module.js';
+
+console.log(add(5,4));
+console.log(subtract(99,100));
+
