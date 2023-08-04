de91240f56f72587e24bab6aafda3ec59adb8089
Nikita Degtyarev <nikita.deki4@gmail.com>
2023-08-04 14:41:34 +0300

    Изменил с учетом, что теперь функции и переменный в разных файлах

--- 2/day17/main.js
+++ 2/day17/main.js
@@ -1,23 +1,7 @@
-let h_inputConteiner = document.getElementById("h_inputConteiner");
-let h_inputField = document.getElementById('h_inputField');
+import { UI_ElEMENTS } from "./view.js";
+import {addTask} from "./functions.js";
 
-
-
-
-
-function addTask(value){
-    let newTask = document.createElement('div');
-    newTask.className = 'newTask';
-    newTask.innerHTML = `<p class="newTaskText">${value}</p>`;
-    h_inputConteiner.append(newTask)
-    h_inputField.value = "";
-}
-
-
-
-
-
-h_inputConteiner.addEventListener('submit', (event)=>{
+UI_ElEMENTS.HIGH_INPUT_CONTEINER.addEventListener('submit', (event)=>{
     event.preventDefault();
-    addTask(h_inputField.value)
+    addTask(UI_ElEMENTS.HIGH_INPUT_FIELD.value);
 })
