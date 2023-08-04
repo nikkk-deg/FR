9b1bfa8ec1ef5c3e2699ca912a7dbd7d8e64a320
Nikita Degtyarev <nikita.deki4@gmail.com>
2023-08-04 14:40:17 +0300

    Создал файл, где будут все переменные

---
+++ 2/day17/view.js
@@ -0,0 +1,9 @@
+export const UI_ElEMENTS = {
+    MAIN_CONTENT: document.getElementById('mainContent'),
+    HIGH_INPUT_CONTEINER: document.getElementById('h_inputConteiner'),
+    LOW_INPUT_CONTEINER: document.getElementById('l_inputConteiner'),
+    HIGH_INPUT_FIELD: document.getElementById('h_inputField'),
+    LOW_INPUT_FIELD: document.getElementById('l_inputField'),
+    HIGH_ARTICLE: document.getElementById('h_article'),
+    LOW_ARTICLE: document.getElementById('l_article'),
+}
