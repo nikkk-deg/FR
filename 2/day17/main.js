c4faae18f3220a8b62e88b292f8cfe93d084706f
Nikita Degtyarev <nikita.deki4@gmail.com>
2023-08-05 21:21:07 +0300

    работает

--- 2/day17/main.js
+++ 2/day17/main.js
@@ -1,6 +1,7 @@
 import { UI_ElEMENTS } from "./view.js";
 import {addTask} from "./functions.js";
 
+
 UI_ElEMENTS.HIGH_INPUT_CONTEINER.addEventListener('submit', (event)=>{
     event.preventDefault();
     addTask(UI_ElEMENTS.HIGH_INPUT_FIELD.value);
