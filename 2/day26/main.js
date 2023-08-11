2c65f10e82dc53a10f9ba8a6d1af45da3df564ca
Nikita Degtyarev <nikita.deki4@gmail.com>
2023-08-11 10:16:21 +0300

    теперь меняется значок погоды

--- 2/day26/main.js
+++ 2/day26/main.js
@@ -3,6 +3,17 @@ import { UI_ELEMENTS, PERMANENTS } from "./view.js";
 
 const getURL = cityName =>`${PERMANENTS.SERVER_URL}?q=${cityName}&appid=${PERMANENTS.API_KEY}`;
 
+const changeToFdegrees = degrees => Math.round(degrees-273);
+
+const change_temp = temp => UI_ELEMENTS.TEMP.textContent = temp;
+
+const changeCurrentFavCity = city => UI_ELEMENTS.CURRENT_FAV_CITY.textContent = city;
+
+const changeWeatherIcon = data => {
+    UI_ELEMENTS.WEATHER_ICON.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    UI_ELEMENTS.ADD_TO_FAV.style.marginTop = '-28px'
+}
+
 const getJSON = url => fetch(url)
     .then(response => {
         if(response.status === 404){
@@ -11,20 +22,15 @@ const getJSON = url => fetch(url)
         return response.json();
     })
     .then(data => {
+        console.log(data);
         change_temp(changeToFdegrees(data.main.temp));
-        changeCurrentFavCity(formatCity(UI_ELEMENTS.SС_FIELD.value));
+        changeCurrentFavCity(data.name);
+        changeWeatherIcon(data);        
         UI_ELEMENTS.SС_FIELD.value = "";
+
     })
     .catch(error => alert(error));
-
-
-const changeToFdegrees = degrees => Math.round(degrees-273);
-
-const change_temp = temp => UI_ELEMENTS.TEMP.textContent = temp;
-
-const changeCurrentFavCity = city => UI_ELEMENTS.CURRENT_FAV_CITY.textContent = city;
-
-const formatCity = city => city.charAt(0).toUpperCase() + city.slice(1);
+// https://openweathermap.org/img/wn/04d@2x.png
 
 UI_ELEMENTS.SC_FORM.addEventListener('submit', event => {
     event.preventDefault();
