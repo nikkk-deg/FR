7733faaec9803bf739d5b2602cdd5253791d28b5
Nikita Degtyarev <nikita.deki4@gmail.com>
2023-10-08 19:04:34 +0300

    123

---
+++ 4/day8/main.js
@@ -0,0 +1,7 @@
+// const greet = (name: string) => alert(`Hi, ${name}`);
+function setNumber(num) {
+    num = 42;
+}
+var ten = 12;
+setNumber(ten);
+console.log(ten);
---
+++ 4/day8/main.ts
@@ -0,0 +1,11 @@
+
+// const greet = (name: string) => alert(`Hi, ${name}`);
+
+function setNumber(num: number){
+    num = 42;
+}
+
+
+let ten = 12;
+setNumber(ten);
+console.log(ten);
