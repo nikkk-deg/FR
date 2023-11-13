ee8c230f0976908b6b5551f7a08a381cdb1d51bb
Nikita Job PC <nikita.deki4@gmail.com>
2023-11-13 17:38:46 +0300

    day 4 has complited

--- 5/react_test/src/Form.js
+++ 5/react_test/src/Form.js
@@ -1,17 +1,20 @@
 
-const formClass = "examples";
-const titleClass = "titleLogin";
-
-
+// const formClass = "examples";
+// const titleClass = "titleLogin";
+// import {useState} from 'react';
 
 export default function Form(){
-  const onSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    alert('hi!')
+    const login = e.target.login.value;
+    const password = e.target.password.value
+    console.log(`логин - ${login};  пароль - ${password}`);
+    e.target.reset();
   }
   return(
-    <form onSubmit={onSubmit}>
-      <input type="text"></input>
+    <form onSubmit={handleSubmit}>
+      <input id = "login" name = "login" type="text"></input>
+      <input id = "password" name = "password" type="password"></input>
       <button type="submit">Отправить</button>
     </form>
   )
