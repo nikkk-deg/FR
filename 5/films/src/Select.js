49ce3eb6dd293e436b27c971ea0d84ba625fa7e7
Nikita Job PC <nikita.deki4@gmail.com>
2023-12-05 10:34:10 +0300

    сделал день 16, нет только стилей

--- 5/films/src/Select.js
+++ 5/films/src/Select.js
@@ -1,23 +1,48 @@
+
+const sortBy = [
+    {value: 'популярности', key: 'popular'},
+    {value: 'убыванию рейтинга', key: 'high_raiting'},
+    {value: 'возрастанию рейтинга', key: 'low_raiting'},
+]
+
+const releaseYears = [];
+for (let i=1900; i<=Number(new Date().getFullYear()); i++){
+    let year = {
+        key: `year${i}`,
+        value: i,
+    }
+    releaseYears.push(year);
+}
+console.log(releaseYears);
+
+
+
 export function Select({isReleaseYear}){
+
+    const showYears = releaseYears.reverse().map(item => {
+        return(
+            <option value={item.value} key={item.key}>{item.value}</option>
+        );
+    })
+
+    const showSortBy = sortBy.map(item => {
+        return(
+            <option value={item.value} key={item.key}>{item.value}</option>
+        )
+    })
     
     if (isReleaseYear){
         return(
             <>
             <label htmlFor = "sorting_select_year">Выбор года релиза:</label>
-            <select name="sort" id = "sorting_select_year">
-            <option value="2023" key="year2023">2023</option>
-            <option value="2022" key="year2022">2022</option>
-            </select>
+            <select name="sort" id = "sorting_select_year">{showYears}</select>
             </>
         );
     }else{
         return(
             <>
             <label htmlFor = "sorting_select_raiting">Сортировать по:</label>
-            <select name="sort" id = "sorting_select_raiting">
-            <option value="popular" key="popular">по популярности</option>
-            <option value="raiting" key="raiting">По рейтингу</option>
-            </select>
+            <select name="sort" id = "sorting_select_raiting">{showSortBy}</select>
             </>
         );
     }
