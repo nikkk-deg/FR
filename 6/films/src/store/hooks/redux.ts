93d167c0285dafac76e91a649424078289b10384
Nikita-home-pc <nikita.deki4@gmail.com>
2024-01-31 17:22:59 +0300

    home commit

--- 6/films/src/store/hooks/redux.ts
+++ 6/films/src/store/hooks/redux.ts
@@ -5,3 +5,4 @@ import { useSelector } from "react-redux";
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 //comment from E570
+//commrnt from home
